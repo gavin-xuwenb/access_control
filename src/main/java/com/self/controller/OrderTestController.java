d188f3701e6011b3adbe52676fd0455d9c1c7365
xuwenbin <563346165@163.com>
2020-11-13 09:23:34 +0800

    TCPclient 修改

--- src/main/java/com/self/controller/OrderTestController.java
+++ src/main/java/com/self/controller/OrderTestController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.self.tcp.TCPClient;
 import com.self.utils.JSONResult;
-import com.self.utils.TCPClient;
 import com.self.utils.nettyClient.SocketClient;
 
 import cn.hutool.json.JSONObject;
